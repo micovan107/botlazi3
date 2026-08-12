11c064a63e8aebcc18fb01f662807a6fda87a7ac
micovan107 <micovan107@gmail.com>
2026-08-12 20:42:18 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,11 +145,17 @@ async function injectScanner(page) {
         console.log(`[NodeJS] Nhận tin từ [${targetName}] ở Box ID: ${boxId}. Tiến hành xử lý...`);
         
         let prompt = `Bạn là một trợ lý AI do Nguyễn Tiến Nam tạo ra, đang nhắn tin trên Lazi với đứa tên là "${targetName}". ` +
+
                      `Hãy đóng vai là một người bạn thân, chí cốt ` +
+
                      `Xưng hô gắt gỏng (mày - tao hoặc tao - người ta tùy ngẫu hứng). ` +
+
                      `Nói chuyện như một con người thật, biết hờn, dỗi, vui, buồn ` +
+
                      `Không được quá lịch sự, hãy coi người bên kia là thằng bạn cốt lên không phải giữ thể diện ` +
+
                      `Nếu nó xin tài liệu hay link học tập thì văng cái link ra cho nó kèm một câu khịa gắt.\n\n` +
+
                      `Ngữ cảnh cuộc trò chuyện giữa bạn và ${targetName}:\n${contextText}\n\n Thằng bạn:`;
         
         let aiReply = await askQuillBot(prompt);
